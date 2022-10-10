edbb513c80807edf4f7908fc814aca35af4f6c7f
Arman Zarghani-Shiraz'git config --global user.name Arman <zarghani@bu.edu>
2022-10-10 17:52:04 -0400

    added dotenv and file to hold my API key

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 '''
 #import requests
@@ -22,9 +24,22 @@ def get_data()
 #     data = json.loads(url.read().decode())
 #     print(data)
 
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+def myEnvironment():
+    print(f'My id is: {api_key}.')
+
+#print(f"my api key is {api_key}")
+
+if __name__ == "__main__":
+    myEnvironment()
+
 def getID():
-    response = requests.get("https://api.watchmode.com/v1/search/?apiKey=am5FlyMQOTybvoxxryFtkswWcNv8oADZKACwViSN&search_field=name&search_value=Back%20to%20the%20Future")
-    
+    #response = requests.get('https://api.watchmode.com/v1/search/?apiKey=am5FlyMQOTybvoxxryFtkswWcNv8oADZKACwViSN&search_field=name&search_value=Back%20to%20the%20Future')
+    response = requests.get(f"https://api.watchmode.com/v1/search/?apiKey={api_key}&search_field=name&search_value=Back%20to%20the%20Future")
+
     output = []
     for movies in response.json()['title_results']:
         output.append(movies['id'])
@@ -38,4 +53,4 @@ def getID():
 
 # Testing
 if __name__ == "__main__":
-    getID()
+    print(getID())
